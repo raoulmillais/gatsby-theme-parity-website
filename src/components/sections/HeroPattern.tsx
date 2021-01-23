b0678c6ba34d41eb951a14453a4837daa017ed61
Imad Arain <arain.imad@gmail.com>
2021-01-23 18:03:21 +0100

    added parallax to heropattern

--- src/components/sections/HeroPattern.tsx
+++ src/components/sections/HeroPattern.tsx
@@ -6,7 +6,7 @@ export default function HeroPattern() {
   const intl = useIntl();
 
   return (
-    <section className="md:bg-hero-pattern bg-cover bg-center -mt-32">
+    <section className="md:bg-hero-pattern bg-cover bg-center bg-fixed -mt-32 md:-mb-32">
       <div className="md:h-screen">
         <div className="container h-full text-center pt-40 md:pt-0 md:pb-0 pb-16 px-2 md:flex md:flex-col md:justify-center md:w-2/3">
           <h3 className="md:text-4xl">{intl.formatMessage({ id: 'homepage-hero-title' })}</h3>
