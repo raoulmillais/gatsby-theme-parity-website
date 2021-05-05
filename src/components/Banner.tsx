55f977c8b88317601fc0f719fa4691057f2e35fa
Imad Arain <arain.imad@gmail.com>
2021-05-05 16:45:08 +0200

    bug fix for Banner Component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import bannerData from '../../content/banner/banner.json';
 
 export default function Banner() {
+  const [currentPath, setCurrentPath] = useState(' ');
+
+  useEffect(() => {
+    setCurrentPath(location.pathname);
+  }, [currentPath]);
+
   return (
     <div
       className={` ${
-        bannerData.active && bannerData.pages.includes(location.pathname) ? `block` : `hidden`
+        bannerData.active && bannerData.pages.includes(currentPath) ? `block` : `hidden`
       } bg-textDark text-parityWhite tracking-wide text-center text-sm px-6 py-3`}
     >
       <p className="mb-0">
